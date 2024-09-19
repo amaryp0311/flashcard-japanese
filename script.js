57102cb0dde49fca79388b54be66f83b4ff44de7
AMaryP <pimlapasairin34@gmail.com>
2024-09-19 20:00:36 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//data
+const hiraganaData = [
+    { question: "あ", correct: "a" 
+    },
+    { question: "い", correct: "i" 
+    },
+    { question: "う", correct: "u" 
+    },
+    { question: "え", correct: "e"
+    },
+];
+
+//process
+function startGame(category) {
+    currentQuestionIndex = 0;
+    score = 0;
+    wrongCount = 0;
+    if (category == "hiragana") {
+        
+    } else if (category == "katakana") {
+        
+    } else if (category == "kanji_n4") {
+        
+    } else if (category == "kanji_n5") {
+        
+    }
+}
+
